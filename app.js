901ba0ac74f89c806bf9b5515858f6293530749c
Dylan Westland <dylanwestland@gmail.com>
2023-06-10 14:35:23 -0700

    working with firebase

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyAIDrJ11LZoum8jtFwAdtqLXIrwnwWX0l4",
+  authDomain: "insiderivy-a6d95.firebaseapp.com",
+  databaseURL: "https://insiderivy-a6d95-default-rtdb.firebaseio.com",
+  projectId: "insiderivy-a6d95",
+  storageBucket: "insiderivy-a6d95.appspot.com",
+  messagingSenderId: "69856689724",
+  appId: "1:69856689724:web:d022e600f19702dd31cd39",
+  measurementId: "G-18GL05TSXZ"
 };
 
 // Initialize Firebase
@@ -22,21 +23,46 @@ function submitForm(e) {
 
   //   Get input Values
   let firstName = document.querySelector(".firstName").value;
-  let lastName = document.querySelector(".lastName").value; // ///////////////////////////// Add variables here
+  let lastName = document.querySelector(".lastName").value;
+  let age = document.querySelector(".age").value;
+  let grade = document.querySelector(".grade").value;
+  let ethnicity = document.querySelector(".ethnicity").value;
+  let state = document.querySelector(".state").value;
+  let zip = document.querySelector(".zip").value; 
+  let email = document.querySelector(".email").value; 
+  let phone = document.querySelector(".phone").value; 
   
-  console.log(firstName, lastName);
+  let unweightedGPA = document.querySelector(".unweightedGPA").value; 
+  let weightedGPA = document.querySelector(".weightedGPA").value;
+  let actScore = document.querySelector(".actScore").value;
+  let satScore = document.querySelector(".satScore").value;
 
-  saveContactInfo(firstName, lastName); // ///////////////////////////// Add variables here
+      // ///////////////////////////// Add variables here
+  
+  console.log(firstName, lastName, age, grade, ethnicity, state, zip, email, phone, unweightedGPA, weightedGPA, actScore, satScore);
+
+  saveContactInfo(firstName, lastName, age, grade, ethnicity, state, zip, email, phone, unweightedGPA, weightedGPA, actScore, satScore); // ///////////////////////////// Add variables here
 
   document.querySelector(".contact-form").reset();
 }
 
 // Save infos to Firebase
-function saveContactInfo(firstName, lastName) { // ///////////////////////////// Add variables here
+function saveContactInfo(firstName, lastName, age, grade, ethnicity, state, zip, email, phone, unweightedGPA, weightedGPA, actScore, satScore) { // ///////////////////////////// Add variables here
   let newContactInfo = contactInfo.push();
 
   newContactInfo.set({
     firstName: firstName,
-    lastName: lastName, // ///////////////////////////// Add variables here
+    lastName: lastName,
+    age: age,
+    grade: grade,
+    ethnicity: ethnicity,
+    state: state,
+    zip: zip,
+    email: email,
+    phone: phone,
+    unweightedGPA: unweightedGPA,
+    weightedGPA: weightedGPA,
+    actScore: actScore,
+    satScore: satScore, // ///////////////////////////// Add variables here
   });
 }
