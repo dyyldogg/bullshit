e7ce5a3f12c4e682321aecbba2b59fca7c596779
Dylan Westland <dylanwestland@MacBook-Air-6.local>
2023-06-13 13:44:30 -0700

    hello

--- login/index.js
+++ login/index.js
@@ -1,163 +1,158 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyAIDrJ11LZoum8jtFwAdtqLXIrwnwWX0l4",
-    authDomain: "insiderivy-a6d95.firebaseapp.com",
-    databaseURL: "https://insiderivy-a6d95-default-rtdb.firebaseio.com",
-    projectId: "insiderivy-a6d95",
-    storageBucket: "insiderivy-a6d95.appspot.com",
-    messagingSenderId: "69856689724",
-    appId: "1:69856689724:web:d022e600f19702dd31cd39",
-    measurementId: "G-18GL05TSXZ"
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>InsiderIvy - Login</title>
+  <!-- Cool Google Fonts -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Bowlby+One+SC&display=swap" rel="stylesheet">
+  <!-- Bootstrap core CSS -->
+  <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Our stylesheet -->
+  <link rel="stylesheet" type="text/css" href="index.css">
+  <style>
+    .login-with-google-btn {
+      transition: background-color 0.3s, box-shadow 0.3s;
+      padding: 12px 16px 12px 42px;
+      border: none;
+      border-radius: 3px;
+      box-shadow: 0 -1px 0 rgba(0, 0, 0, 0.04), 0 1px 1px rgba(0, 0, 0, 0.25);
+      color: #757575;
+      font-size: 14px;
+      font-weight: 500;
+      font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+      background-image: url("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj48cGF0aCBkPSJNMTcuNiA5LjJsLS4xLTEuOEg5djMuNGg0LjhDMTMuNiAxMiAxMyAxMyAxMiAxMy42djIuMmgzYTguOCA4LjggMCAwIDAgMi42LTYuNnoiIGZpbGw9IiM0Mjg1RjQiIGZpbGwtcnVsZT0ibm9uemVybyIvPjxwYXRoIGQ9Ik05IDE4YzIuNCAwIDQuNS0uOCA2LTIuMmwtMy0yLjJhNS40IDUuNCAwIDAgMS04LTIuOUgxVjEzYTkgOSAwIDAgMCA4IDV6IiBmaWxsPSIjMzRBODUzIiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNOSAzLjZjMS4zIDAgMi41LjQgMy40IDEuM0wxNSAyLjNBOSA5IDAgMCAwIDEgNWwzIDIuNGE1LjQgNS40IDAgMCAxIDUtMy43eiIgZmlsbD0iI0VBNDMzNSIgZmlsbC1ydWxlPSJub256ZXJvIi8+PHBhdGggZD0iTTAgMGgxOHYxOEgweiIvPjwvZz48L3N2Zz4=");
+      background-color: white;
+      background-repeat: no-repeat;
+      background-position: 12px 11px;
+    }
+
+    .login-with-google-btn:hover {
+      box-shadow: 0 -1px 0 rgba(0, 0, 0, 0.04), 0 2px 4px rgba(0, 0, 0, 0.25);
+    }
+
+    .login-with-google-btn:active {
+      background-color: #eeeeee;
+    }
+
+    .login-with-google-btn:focus {
+      outline: none;
+      box-shadow: 0 -1px 0 rgba(0, 0, 0, 0.04), 0 2px 4px rgba(0, 0, 0, 0.25), 0 0 0 3px #c8dafc;
+    }
+
+    .login-with-google-btn:disabled {
+      filter: grayscale(100%);
+      background-color: #ebebeb;
+      box-shadow: 0 -1px 0 rgba(0, 0, 0, 0.04), 0 1px 1px rgba(0, 0, 0, 0.25);
+      cursor: not-allowed;
+    }
+  </style>
+</head>
+<body class="bg-light">
+<header>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-success">
+    <div class="container-fluid px-md-3">
+      <a class="navbar-brand" href="#">InsiderIvy</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+              aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav ms-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">My Account</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
+
+<div id="content_container">
+  <div id="form_container">
+    <div id="form_header_container">
+      <h2 id="form_header">Login to InsiderIvy</h2>
+    </div>
+
+    <div id="form_content_container">
+      <div id="form_content_inner_container">
+        <input type="email" id="email" placeholder="Email">
+        <input type="password" id="password" placeholder="New Password">
+
+        <div id="button_container">
+          <button onclick="login()">Login</button>
+        </div>
+
+        <div id="google_button_container">
+          <div class="g-signin2" data-onsuccess="onSignIn"></div>
+        </div>
+
+        <div id="register_container">
+          <p id="register_text">Don't have an account? <a href="#" style="color: green;">Register here</a></p>
+          <p id="forgot_password"><a href="#" style="color: green;">Forgot your password? Reset it here.</a></p>
+        </div>
+
+        <button type="button" class="login-with-google-btn" disabled>
+          Sign in with Google
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.6.8/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.6.8/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.6.8/firebase-database.js"></script>
+
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+<script src="index.js"></script>
+
+<script>
+  function onSuccess(googleUser) {
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+  }
+
+  function onFailure(error) {
+    console.log(error);
+  }
+
+  function initializeGoogleSignIn() {
+    gapi.load('auth2', function() {
+      gapi.auth2.init({
+        client_id: 'YOUR_CLIENT_ID.apps.googleusercontent.com'
+      }).then(function() {
+        gapi.signin2.render('google_button_container', {
+          'scope': 'profile email',
+          'width': 240,
+          'height': 50,
+          'longtitle': true,
+          'theme': 'dark',
+          'onsuccess': onSuccess,
+          'onfailure': onFailure
+        });
+      });
+    });
+  }
+
+  window.onload = function() {
+    initializeGoogleSignIn();
   };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  // Initialize variables
-  const auth = firebase.auth();
-  const database = firebase.database();
-  
-  // Set up our register function
-  function register() {
-    // Get all our input fields
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-  
-    // Validate input fields
-    if (validate_email(email) == false || validate_password(password) == false) {
-      alert('Email or Password is Invalid!');
-      return;
-      // Don't continue running the code
-    }
-  
-    // Move on with Auth
-    auth.createUserWithEmailAndPassword(email, password)
-      .then(function() {
-        // Declare user variable
-        var user = auth.currentUser;
-  
-        // Add this user to Firebase Database
-        var database_ref = database.ref();
-  
-        // Create User data
-        var user_data = {
-          email: email,
-          last_login: Date.now()
-        };
-  
-        // Push to Firebase Database
-        database_ref.child('users/' + user.uid).set(user_data);
-  
-        // Done
-        alert('User Created!');
-      })
-      .catch(function(error) {
-        // Firebase will use this to alert of its errors
-        var error_code = error.code;
-        var error_message = error.message;
-  
-        alert(error_message);
-      });
-  }
-  
-  // Set up our login function
-  function login() {
-    // Get all our input fields
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-  
-    // Validate input fields
-    if (validate_email(email) == false || validate_password(password) == false) {
-      alert('Email or Password is Invalid!');
-      return;
-      // Don't continue running the code
-    }
-  
-    auth.signInWithEmailAndPassword(email, password)
-      .then(function() {
-        // Declare user variable
-        var user = auth.currentUser;
-  
-        // Add this user to Firebase Database
-        var database_ref = database.ref();
-  
-        // Create User data
-        var user_data = {
-          last_login: Date.now()
-        };
-  
-        // Push to Firebase Database
-        database_ref.child('users/' + user.uid).update(user_data);
-  
-        // Done
-        alert('User Logged In!');
-      })
-      .catch(function(error) {
-        // Firebase will use this to alert of its errors
-        var error_code = error.code;
-        var error_message = error.message;
-  
-        alert(error_message);
-      });
-  }
-  
-  // Validate Functions
-  function validate_email(email) {
-    var expression = /^[^@]+@\w+(\.\w+)+\w$/;
-    if (expression.test(email) == true) {
-      // Email is good
-      return true;
-    } else {
-      // Email is not good
-      return false;
-    }
-  }
-  
-  function validate_password(password) {
-    // Firebase only accepts lengths greater than 6
-    if (password.length < 6) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-  
-  function validate_field(field) {
-    if (field == null) {
-      return false;
-    }
-  
-    if (field.length <= 0) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-  
-  // Add event listener to input fields for Enter key
-//   var displayNameInput = document.getElementById("displayName"); 
-/////// Im deciding not to put display name here
+</script>
 
-
-  var emailInput = document.getElementById("email");
-  var passwordInput = document.getElementById("password");
-  
-//   displayNameInput.addEventListener("keyup", function(event) {
-//     if (event.keyCode === 13) { // Enter key code
-//       event.preventDefault();
-//       login(); 
-//     }
-//   });
-  
-  emailInput.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-      event.preventDefault();
-      login();
-    }
-  });
-  
-  passwordInput.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-      event.preventDefault();
-      login();
-    }
-  });
-  
+<script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
